179c9a843cf18abcacb77d041ddaf6d96ef4a16b
MuhamadFaizMutalib <muhamad.faiz.mutalib@gmail.com>
2025-05-01 21:32:22 +0800

    Description of your changes

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ async function initDB() {
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
+        role VARCHAR(50) DEFAULT 'public',
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
